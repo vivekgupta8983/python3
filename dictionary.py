581ad97290ed85954dc31c7d00dbd309950bd98b
vivekgupta8983 <vivekgupta8983@gmail.com>
2020-03-19 14:43:24 +0530

    #Dictionary is a collection of data which is unordered, changeable, indexed. In python it is written in curl brackets {} # It has Key and values

--- dictionary.py
+++ dictionary.py
@@ -5,13 +5,22 @@
 
 car = { 'Name':'car', 'Make':'Audi', 'Year':2020, 'color':'red' }
 
-print(car['color'])
+# print(car['color'])
 
-car['cc'] = 650
-print(car)
+# car['cc'] = 650
+# print(car)
 
-del car['Name']  # delete only Index Item
+# del car['Name']  # delete only Index Item
 
-del car # delete whole dictionary
+# del car # delete whole dictionary
 
-print(car)
+# print(car)
+
+
+# while True:
+#     key = input("Please enter your key:")
+#     if key == 'quit':
+#         break
+#     keyvalue = car.get(key, f'{key} Does not exists in this car')
+#     print(keyvalue)
+
