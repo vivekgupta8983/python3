6542131a9748a64c602f1c6d46a1e6f46c4e7663
vivekgupta8983 <vivekgupta8983@gmail.com>
2020-03-19 12:53:54 +0530

    List are sequences of items list constructor method to call constructor

--- list.py
+++ list.py
@@ -5,12 +5,27 @@
 # for letter in list_of_letter:
 #     print("I have got the Letter " + letter)
 
-even_list = ['2', '4', '6', '8']
-odd_list = ['3', '5', '7', '9']
+# even_list = ['2', '4', '6', '8']
+# odd_list = ['3', '5', '7', '9']
 
-num = even_list +odd_list
-num.sort()
-print(num)
+# num = even_list +odd_list
+# num.sort()
+# print(num)
 
-x = sorted(num)
-print(x)
+# x = sorted(num)
+# print(x)
+
+list_1 = []
+list_2 = list()
+
+print(list_1)
+print(list_2)
+
+if list_1 == list_2:
+    print("Both Lists are equal")
+
+list_2 = list('This is a Parameters')  #constructor method to call constructor
+print(list_2)
+
+
+ 
