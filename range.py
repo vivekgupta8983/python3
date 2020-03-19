63384f9ce44c78056b028b5e8fb9a70f61d559a7
vivekgupta8983 <vivekgupta8983@gmail.com>
2020-03-19 13:35:15 +0530

    #range functions returns a sequence for number start from o and ends ta specific number  #range(start, stop, step)

---
+++ range.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#range functions returns a sequence for number start from o and ends ta specific number 
+#range(start, stop, step)
+
+# for i in range(100):
+#     print(i)
+
+# for i in range(2, 10, 2):
+#     print(i)
+
+my_range = range(1, 10)
+print(my_range.index(3))
+
