fb68eb28e4e1d6e1a7345473dc32922299b031e2
vivekgupta8983 <vivekgupta8983@gmail.com>
2020-03-19 10:36:45 +0530

    Augument Assignment Operator

--- PTVSD_LAUNCHER_PORT=45699/Augument Assignment.py
+++ PTVSD_LAUNCHER_PORT=45699/Augument Assignment.py
@@ -1,12 +1,71 @@
 #!/usr/bin/python3
 
-num = "1,123,1234,123445,123,123,56,767,87,9"
+# num = "1,123,1234,123445,123,123,56,767,87,9"
 
-clnnum = ""
+# clnnum = ""
 
-for i in range(0, len(num)):
-    if num [i] in "123456789":
-        clnnum1 = clnnum + num[i]
-        clnnum += num[i]          # augument assignment
-print(clnnum1)
-print(clnnum)
+# for i in range(0, len(num)):
+#     if num [i] in "123456789":
+#         clnnum1 = clnnum + num[i]
+#         clnnum += num[i]          # augument assignment
+# print(clnnum1)
+# print(clnnum)
+
+# num = '1,123,456,789,365,256'
+#
+# cln_num = ''
+#
+# for i in range(0,len(num)):
+#     if num[i] in '123456789':
+#         cln_num +=  num[i]
+#
+# print(cln_num)
+
+x = 10
+x += 1
+print(x)
+
+x -= 4
+print(x)
+
+x *= 4
+print(x)
+
+x /= 4
+print(x)
+
+
+x //= 4
+print(x)
+
+x %= 4
+print(x)
+
+
+
+
+message = 'Hi'
+message += ' Vivek'
+message *= 5
+print(message)
+
+
+# +=  -=  *=  /=  **=  %=  >>=  <<=  &=  ^=  |=
+
+
+
+
+
+# ip = input('Please enter IP adess: ')
+
+# segment = 1
+# length = 0
+
+# for char in ip:
+#     if char == '.':
+#         print(f'Now length is {length} there are {segment}')
+#         segment += 1
+#         length = 0
+#     else:
+#         length += 1
+#         print(length)
