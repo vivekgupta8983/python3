c0a1b165be78711209783f606b775e24da249af1
vivekgupta8983 <vivekgupta8983@gmail.com>
2020-03-19 12:41:50 +0530

    list example list is squence of Items

---
+++ PTVSD_LAUNCHER_PORT=45699/list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# list_of_letter = ['A', 'B', 'C', 'D']
+# list_of_letter.append('E')
+# for letter in list_of_letter:
+#     print("I have got the Letter " + letter)
+
+even_list = ['2', '4', '6', '8']
+odd_list = ['3', '5', '7', '9']
+
+num = even_list +odd_list
+num.sort()
+print(num)
