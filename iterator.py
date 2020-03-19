9a79b28910aca5594845f0b1431f72526a6e0df0
vivekgupta8983 <vivekgupta8983@gmail.com>
2020-03-19 13:20:06 +0530

    Itearator functions are repitating similar task without makeing anf errors

--- iterator.py
+++ iterator.py
@@ -4,6 +4,11 @@ string = '123456789'
 # for char in string:
 #     print(char)
 
+
+#Itearator functions are repitating similar task without makeing anf errors
+
+
+
 my_iterator = iter(string)
 print(next(my_iterator))
 print(next(my_iterator))
