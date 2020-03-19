5d321f2f19279e721f46f0d2b41d5054403f18b1
vivekgupta8983 <vivekgupta8983@gmail.com>
2020-03-19 15:02:01 +0530

    Sets is collection of unordered and unindexed. It is written in {} curly brackes and set functions

---
+++ sets.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+#Sets is collection of unordered and unindexed. It is written in {} curly brackes and set functions
+
+# set_a = {'A', 'B', 'C'}  # Define a set using {} curly brackets
+# set_b = set(['A', 'B', 'C'])    # Define a set using set functions
+
+# print(set_a)
+# print(set_b)
+
+odd_set = set(range(1,10,2))
+even_set = set(range(2,10,2))
+
+print(odd_set)
+print(even_set)
